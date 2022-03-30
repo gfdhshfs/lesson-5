8eaf0ce60207448b734df2e2e7c8ea0e7ea9d666
unknown <ilya.zakharov.2018@mail.ru>
2022-03-30 09:51:21 +0300

    okey

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using static Library.LoginServise;
+using Library;
+
+namespace Tests
+{
+    [TestClass]
+    public class LoginTest
+    {
+        [TestMethod]
+        public void TestLoginMethod()
+        {
+            var login = "admin";
+            var password = "admin";
+
+            var LoginStatus = LoginServise.Login(login, password);
+        }
+    }
+}
---
+++ Учет абонементов/Pages/Loginpages.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Учет_абонементов.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Loginpages.xaml
+    /// </summary>
+    public partial class Loginpages : Page
+    {
+        public Loginpages()
+        {
+            InitializeComponent();
+
+            private void Button_Click(object sender, RoutedEventArgs e)
+            {
+                var login = LoginTextBox.Text.Trim();
+
+                var password = PasswordTextBox.Text.Trim();
+
+                var loginStatus = LoginServise.Login(login, password);
+
+           
+            }
+
+        }
+    }
+}
